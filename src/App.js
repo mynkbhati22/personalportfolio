be353accd76e2638966e923cb470a4f84dc9aa40
mynkbhati22 <mayankbhati1911@gmail.com>
2022-11-12 17:02:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import ReactNavbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <ReactNavbar />
+      <Router>
+        <ReactNavbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import bg from "../assets/img/banner-bg.png";
+import { FaLinkedinIn, FaTelegramPlane, FaInstagram } from "react-icons/fa";
 
 export default function ReactNavbar() {
   const [active, setActive] = React.useState(0);
@@ -26,31 +28,54 @@ export default function ReactNavbar() {
         <Navbar.Brand href="#home">MY PROTFOLIO</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+          <Nav className="mx-auto">
             <Nav.Link
               href="#home"
-              className={`${active === 1 ? "active" : ""}`}
+              className={`${active === 1 ? "active" : ""}mx-2`}
               onClick={() => setActive(1)}
             >
               Home
             </Nav.Link>
             <Nav.Link
               href="#skills"
-              className={`${active === 2 ? "active" : ""}`}
+              className={`${active === 2 ? "active" : ""} mx-2`}
               onClick={() => setActive(2)}
             >
               Skills
             </Nav.Link>
             <Nav.Link
               href="#my-portfolio"
-              className={`${active === 3 ? "active" : ""}`}
+              className={`${active === 3 ? "active" : ""} mx-2`}
               onClick={() => setActive(3)}
             >
               Portfolio
             </Nav.Link>
+            <Nav.Link
+              href="#"
+              target="_blank"
+              className={`${active === 4 ? "active" : ""} mx-2`}
+              onClick={() => setActive(4)}
+            >
+              Contact Us
+            </Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link href="/" target="_blank" className="sociallink">
+              <FaTelegramPlane />
+            </Nav.Link>
+            <Nav.Link href="/" target="_blank" className="sociallink">
+              <FaLinkedinIn />
+            </Nav.Link>
+            <Nav.Link href="/" target="_blank" className="sociallink">
+              <FaInstagram />
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
+
+      <div className="background">
+        <img src={bg} alt="" className="bg-img" />
+      </div>
     </Navbar>
   );
 }
